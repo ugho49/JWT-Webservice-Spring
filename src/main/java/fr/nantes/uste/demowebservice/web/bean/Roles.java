af7ae1c96bcdb7bfcb6a7015127137f75d1ae161
Ugho Stephan <stephan.ugho@gmail.com>
2017-06-24 02:30:21 +0200

    add PreAuthorize

--- src/main/java/fr/nantes/uste/demowebservice/web/bean/Roles.java
+++ src/main/java/fr/nantes/uste/demowebservice/web/bean/Roles.java
@@ -4,6 +4,17 @@ package fr.nantes.uste.demowebservice.web.bean;
  * Created by ughostephan on 23/06/2017.
  */
 public enum Roles {
-    ROLE_USER,
-    ROLE_ADMINISTRATOR
+    ROLE_USER("ROLE_USER"),
+    ROLE_ADMINISTRATOR("ROLE_ADMINISTRATOR");
+
+    private final String name;
+
+    Roles(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
--- src/main/java/fr/nantes/uste/demowebservice/web/controller/UserController.java
+++ src/main/java/fr/nantes/uste/demowebservice/web/controller/UserController.java
@@ -1,10 +1,12 @@
 package fr.nantes.uste.demowebservice.web.controller;
 
-import fr.nantes.uste.demowebservice.web.util.DataEnvelop;
 import fr.nantes.uste.demowebservice.web.bean.User;
 import fr.nantes.uste.demowebservice.web.service.UserService;
+import fr.nantes.uste.demowebservice.web.util.DataEnvelop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
  * Created by ughostephan on 23/06/2017.
  */
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -21,12 +23,14 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping()
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
     public DataEnvelop getUsers() {
         return DataEnvelop.CreateEnvelop(userService.getAll());
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/user/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
     public DataEnvelop getUserById(@PathVariable(name = "id") String id) {
         final User u = userService.getById(id);
 
@@ -37,12 +41,26 @@ public class UserController {
         return DataEnvelop.CreateEnvelop(HttpStatus.NO_CONTENT, "User not found");
     }
 
-    @PostMapping("")
+    @GetMapping("/user")
+    public DataEnvelop getCurrentUser() {
+        final User userFromToken = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        //final User user = userService.getById(userFromToken.getUid());
+
+        if (userFromToken != null) {
+            return DataEnvelop.CreateEnvelop(userFromToken);
+        }
+
+        return DataEnvelop.CreateEnvelop(HttpStatus.NO_CONTENT, "User not found");
+    }
+
+    @PostMapping("/user")
+    @PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
     public DataEnvelop addUser() {
         /*User u = new User();
-        u.setFirstname("Lea");
-        u.setLastname("STEPHAN");
-        u.setEmail("lea.stephan@gmail.com");
+        u.setFirstname("Lxfgea");
+        u.setLastname("ggfxgxfg");
+        u.setEmail("lexgfa.xg@gmail.com");
         u.setPassword(passwordEncoder.encode("test"));
         u.setCreated_at(new Date());
         u.setRole(Roles.ROLE_USER.toString());
